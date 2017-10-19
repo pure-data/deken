41bf7df9e16e2c351b636663b6ec9a50b51bec96
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2017-10-19 08:48:35 +0200

    added boilerplate to deken.hy

--- developer/deken.py
+++ developer/deken.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This software is copyrighted by Chris McCormick and others.  The following
+# terms (the "Standard Improved BSD License") apply to all files associated
+# with the software unless explicitly disclaimed in individual files:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+# 3. The name of the author may not be used to endorse or promote
+#    products derived from this software without specific prior
+#    written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+
+# this is a thin wrapper around deken.hy, to launch it without a
+# HY executable (or file associations)
+
 import os
 import hy
 hy.importer.import_file_to_module("__main__", os.path.join(os.path.dirname(os.path.realpath(__file__)), "deken.hy"))
