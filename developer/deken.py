98b9cf104e11d253567222ffeace26a513b4843b
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2017-10-19 08:50:52 +0200

    Cosmetic clean up of wrapper code

--- developer/deken.py
+++ developer/deken.py
@@ -37,6 +37,6 @@
 
 import os
 import hy
-hy.importer.import_file_to_module("__main__", os.path.join(os.path.dirname(os.path.realpath(__file__)), "deken.hy"))
-#import deken
-# deken.main()
+hy.importer.import_file_to_module(
+    "__main__",
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "deken.hy"))
