b92e774b5ebddb9db962da1f78d293c8c4ec52a6
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2017-10-23 20:14:29 +0200

    Bumped version to 0.2.5
    so we can override the deken that is included in Pd-0.48-1

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -59,7 +59,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.2.4] } {
+if { [::deken::versioncheck 0.2.5] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
