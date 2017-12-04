e92c1a2f20c8e63ed67c5d047c483447e18ffc6c
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2017-12-04 21:14:28 +0100

    just add paths if add:to_path > 1

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -982,9 +982,15 @@ proc ::deken::clicked_link {URL filename} {
             ::pdwindow::debug "\n"
             return
         }
-        set msg [_ "Add %s to the Pd search paths?" ]
-        set _args "-message \"[format $msg $extname]\" -type yesno -default yes -icon question -parent .externals_searchui"
-        switch -- [eval tk_messageBox ${_args}] {
+        set doit yes
+        if { $::deken::add_to_path > 1 } {
+            set doit yes
+        } {
+            set msg [_ "Add %s to the Pd search paths?" ]
+            set _args "-message \"[format $msg $extname]\" -type yesno -default yes -icon question -parent .externals_searchui"
+            set doit [eval tk_messageBox ${_args}]
+        }
+        switch -- "${doit}" {
             yes {
                 add_to_searchpaths [file join $installdir $extname]
                 ::pdwindow::debug "\[deken\]: "
