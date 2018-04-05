6235978a5f1f1d4125125dd4aeb58dd17d3daf74
IOhannes m zmölnig <zmoelnig@iem.at>
2018-04-05 10:36:40 +0200

    released v0.4.1

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -95,7 +95,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.4.0] } {
+if { [::deken::versioncheck 0.4.1] } {
 
 ## FIXXXXME only initialize vars if not yet set
 set ::deken::installpath {}
