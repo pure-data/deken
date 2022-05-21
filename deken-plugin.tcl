218c716ad0df04847bf74fd562f955fb1849b86f
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2022-05-21 16:02:33 +0200

    Releasing v0.9

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -103,7 +103,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.8.1] } {
+if { [::deken::versioncheck 0.9] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
