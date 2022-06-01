df71b9744c3651b744f07058e2ff4d25d8ff2aff
IOhannes m zmölnig <zmoelnig@iem.at>
2022-06-01 11:38:35 +0200

    Bump dev-version to 0.9.3

--- .git-ci/deken-test/README.deken.txt
+++ .git-ci/deken-test/README.deken.txt
@@ -1,7 +1,7 @@
 deken-test
 ==========
 
-version: 0.9.2
+version: 0.9.3
 
 this is just a library for testing the internal workings of the deken framework.
 
--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -103,7 +103,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.9.2] } {
+if { [::deken::versioncheck 0.9.3] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
