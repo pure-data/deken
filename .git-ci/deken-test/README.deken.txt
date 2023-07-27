f708d6d4dba183fa104922485e8b1b7de247b673
IOhannes m zmölnig <zmoelnig@iem.at>
2023-07-27 14:16:07 +0200

    Releasing devek v0.9.10

--- .git-ci/deken-test/README.deken.txt
+++ .git-ci/deken-test/README.deken.txt
@@ -1,7 +1,7 @@
 deken-test
 ==========
 
-version: 0.9.9
+version: 0.9.10
 
 this is just a library for testing the internal workings of the deken framework.
 
--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -103,7 +103,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.9.9] } {
+if { [::deken::versioncheck 0.9.10] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
