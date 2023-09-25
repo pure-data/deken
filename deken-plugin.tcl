3227eb39dd494169c142de4700c5c74c367601ea
IOhannes m zmölnig <zmoelnig@iem.at>
2023-09-25 09:57:15 +0200

    Bump dev-version to v0.9.13

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -103,7 +103,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.9.12] } {
+if { [::deken::versioncheck 0.9.13] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
