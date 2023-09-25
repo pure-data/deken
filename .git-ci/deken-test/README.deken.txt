0f045c6932e8a4c4fbda500502a45f6606ab3f54
IOhannes m zmölnig <zmoelnig@iem.at>
2023-09-25 09:57:06 +0200

    Releasing v0.9.12

--- .git-ci/deken-test/README.deken.txt
+++ .git-ci/deken-test/README.deken.txt
@@ -1,7 +1,7 @@
 deken-test
 ==========
 
-version: 0.9.11
+version: 0.9.12
 
 this is just a library for testing the internal workings of the deken framework.
 
--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -103,7 +103,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.9.11] } {
+if { [::deken::versioncheck 0.9.12] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
