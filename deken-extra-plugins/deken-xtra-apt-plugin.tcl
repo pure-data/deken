f65c7d65809107a5a5223a9a952153c329c5a428
IOhannes m zmölnig <zmoelnig@iem.at>
2024-12-04 11:55:42 +0100

    use "${var}" rather than "$var" for deken-apt as well

--- deken-extra-plugins/deken-xtra-apt-plugin.tcl
+++ deken-extra-plugins/deken-xtra-apt-plugin.tcl
@@ -24,122 +24,123 @@ proc ::deken::apt::search {name} {
 	    set ::deken::apt::distribution {}
 	}
     }
-    if { "$::deken::apt::distribution" == "" } {
+    if { "${::deken::apt::distribution}" == "" } {
 	return
     }
 
-    set name [ string tolower $name ]
+    set name [ string tolower ${name} ]
     array unset pkgs
     array set pkgs {}
     set _dpkg_query {dpkg-query -W -f ${db:Status-Abbrev}${Version}\n}
     set filter "-F Provides pd-externals --or -F Depends -w pd --or -F Depends -w puredata --or -F Depends -w puredata-core"
-    if { "$name" == "" } { } {
-	set filter " -F Package $name --and ( $filter )"
+    if { "${name}" == "" } { } {
+	set filter " -F Package ${name} --and ( ${filter} )"
     }
 
-    set io [ open "|grep-aptavail -n -s Package $filter | sort -u | xargs apt-cache madison" r ]
-    while { [ gets $io line ] >= 0 } {
-        set llin [ split "$line" "|" ]
-        set pkgname [ string trim [ lindex $llin 0 ] ]
+    set io [ open "|grep-aptavail -n -s Package ${filter} | sort -u | xargs apt-cache madison" r ]
+    while { [ gets ${io} line ] >= 0 } {
+        set llin [ split "${line}" "|" ]
+        set pkgname [ string trim [ lindex ${llin} 0 ] ]
 
-        #if { $pkgname ne $searchname } { continue }
-        set ver_  [ string trim [ lindex $llin 1 ] ]
-        set info_ [ string trim [ lindex $llin 2 ] ]
+        #if { ${pkgname} ne ${searchname} } { continue }
+        set ver_  [ string trim [ lindex ${llin} 1 ] ]
+        set info_ [ string trim [ lindex ${llin} 2 ] ]
 
 	## status: is the package installed?
 	set state "Provided"
         set installed 0
 	catch {
 	    set io2 [ open "|${_dpkg_query} ${pkgname} | egrep ^ii | egrep ${ver_}$" ]
-	    if { [ gets $io2 _ ] >= 0 } {
+	    if { [ gets ${io2} _ ] >= 0 } {
 		set state "Already installed"
                 set installed 1
 	    } {
-		while { [ gets $io2 _ ] >= 0 } { }
+		while { [ gets ${io2} _ ] >= 0 } { }
 	    }
 	}
-        if { "Packages" eq [ lindex $info_ end ] } {
-            set suite [ lindex $info_ 1 ]
-            set arch  [ lindex $info_ 2 ]
-            if { ! [ info exists pkgs($pkgname/$ver_) ] } {
-                set pkgs($pkgname/$ver_) [ list $pkgname $ver_ $suite $arch $state $installed]
+        if { "Packages" eq [ lindex ${info_} end ] } {
+            set suite [ lindex ${info_} 1 ]
+            set arch  [ lindex ${info_} 2 ]
+            if { ! [ info exists pkgs(${pkgname}/${ver_}) ] } {
+                set pkgs(${pkgname}/${ver_}) [ list ${pkgname} ${ver_} ${suite} ${arch} ${state} ${installed}]
             }
         }
     }
     foreach {name inf} [ array get pkgs ] {
-        set pkgname [ lindex $inf 0 ]
-        set v       [ lindex $inf 1 ]
-        set suite   [ lindex $inf 2 ]
-        set arch    [ lindex $inf 3 ]
-        set state   [ lindex $inf 4 ]
-        set cmd [list ::deken::apt::install ${pkgname}=$v]
+        set pkgname [ lindex ${inf} 0 ]
+        set v       [ lindex ${inf} 1 ]
+        set suite   [ lindex ${inf} 2 ]
+        set arch    [ lindex ${inf} 3 ]
+        set state   [ lindex ${inf} 4 ]
+        set cmd [list ::deken::apt::install ${pkgname}=${v}]
         set match 1
         set comment "${state} by ${::deken::apt::distribution} (${suite})"
         set status "${pkgname}_${v}_${arch}.deb"
         set contextcmd ""
-        if { [ lindex $inf 5 ] } {
+        if { [ lindex ${inf} 5 ] } {
             set contextcmd [list ::deken::apt::contextmenu %W %x %y ${pkgname}]
         }
-        lappend result [list $name $cmd $match $comment $status $pkgname $v $suite $contextcmd]
+        lappend result [list ${name} ${cmd} ${match} ${comment} ${status} ${pkgname} ${v} ${suite} ${contextcmd}]
     }
 
     # version-sort the results and normalize the result-string
     set sortedresult []
     if {[llength [info procs ::deken::normalize_result ]] > 0} {
-        foreach r [lsort -dictionary -decreasing -index 1 $result ] {
-            foreach {title cmd match comment status pkgname version suite cmd2} $r {break}
-            lappend sortedresult [::deken::normalize_result $title $cmd $match $comment $status $cmd2 $pkgname $version Debian $suite]
+        foreach r [lsort -dictionary -decreasing -index 1 ${result} ] {
+            foreach {title cmd match comment status pkgname version suite cmd2} ${r} {break}
+            lappend sortedresult [::deken::normalize_result ${title} ${cmd} ${match} ${comment} ${status} ${cmd2} ${pkgname} ${version} Debian ${suite}]
         }
     } {
-        foreach r [lsort -dictionary -decreasing -index 1 $result ] {
-            # [list $title $cmd $match $comment $status]
-            foreach {title cmd match comment status} $r {break}
-            lappend sortedresult [list $title $cmd $match $comment $status]
+        foreach r [lsort -dictionary -decreasing -index 1 ${result} ] {
+            # [list ${title} ${cmd} ${match} ${comment} ${status}]
+            foreach {title cmd match comment status} ${r} {break}
+            lappend sortedresult [list ${title} ${cmd} ${match} ${comment} ${status}]
         }
     }
-    return $sortedresult
+    return ${sortedresult}
 }
 
 
 proc ::deken::apt::contextmenu {widget theX theY pkgname} {
     set m .dekenresults_contextMenu
-    destroy $m
-    menu $m
-    $m add command -label [format [_ "Uninstall '%s'" ] $pkgname] -command [list ::deken::apt::uninstall $pkgname]
-    tk_popup $m [expr {[winfo rootx $widget] + $theX}] [expr {[winfo rooty $widget] + $theY}]
+    destroy ${m}
+    menu ${m}
+    ${m} add command -label [format [_ "Uninstall '%s'" ] ${pkgname}] -command [list ::deken::apt::uninstall ${pkgname}]
+    tk_popup ${m} [expr {[winfo rootx ${widget}] + ${theX}}] [expr {[winfo rooty ${widget}] + ${theY}}]
 }
 
 proc ::deken::apt::getsudo {} {
     # for whatever reasons, we cannot have 'deken' as the description
-    # (it will always show $prog instead)
+    # (it will always show ${prog} instead)
     set desc deken::apt
     if { [ catch { exec which pkexec } sudo ] } {
 	if { [ catch { exec which gksudo } sudo ] } {
 	    set sudo ""
-	} { set sudo "$sudo -D $desc --"
+	} {
+            set sudo "${sudo} -D ${desc} --"
 	}
     }
-    if { $sudo == "" } {
+    if { ${sudo} == "" } {
 	::deken::post "Please install 'policykit-1', if you want to install system packages via deken..." error
     }
-    return $sudo
+    return ${sudo}
 }
 
 proc ::deken::apt::install {pkg {version {}}} {
-    if { $version ne {} } {
+    if { ${version} ne {} } {
         set pkg "${pkg}=${version}"
     }
     set prog "apt-get install -y --show-progress ${pkg}"
     set sudo [::deken::apt::getsudo]
-    if { $sudo ne "" } {
-        set cmdline "$sudo $prog"
-        #::deken::post "$cmdline" error
+    if { ${sudo} ne "" } {
+        set cmdline "${sudo} ${prog}"
+        #::deken::post "${cmdline}" error
         set io [ open "|${cmdline}" ]
-        while { [ gets $io line ] >= 0 } {
-            ::deken::post "apt: $line"
+        while { [ gets ${io} line ] >= 0 } {
+            ::deken::post "apt: ${line}"
         }
-        if { [ catch { close $io } ret ] } {
-            ::deken::post "apt::install failed to install $pkg" error
+        if { [ catch { close ${io} } ret ] } {
+            ::deken::post "apt::install failed to install ${pkg}" error
             ::deken::post "\tDid you provide the correct password and/or" error
             ::deken::post "\tis the apt database locked by another process?" error
         }
@@ -149,15 +150,15 @@ proc ::deken::apt::install {pkg {version {}}} {
 proc ::deken::apt::uninstall {pkg} {
     set prog "apt-get remove -y --show-progress ${pkg}"
     set sudo [::deken::apt::getsudo]
-    if { $sudo ne "" } {
-        set cmdline "$sudo $prog"
-        #::deken::post "$cmdline" error
+    if { ${sudo} ne "" } {
+        set cmdline "${sudo} ${prog}"
+        #::deken::post "${cmdline}" error
         set io [ open "|${cmdline}" ]
-        while { [ gets $io line ] >= 0 } {
-            ::deken::post "apt: $line"
+        while { [ gets ${io} line ] >= 0 } {
+            ::deken::post "apt: ${line}"
         }
-        if { [ catch { close $io } ret ] } {
-            ::deken::post "apt::uninstall failed to remove $pkg" error
+        if { [ catch { close ${io} } ret ] } {
+            ::deken::post "apt::uninstall failed to remove ${pkg}" error
             ::deken::post "\tDid you provide the correct password and/or" error
             ::deken::post "\tis the apt database locked by another process?" error
         }
