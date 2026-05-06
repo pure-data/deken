f54a89c8d1b3c77e9d626c992b6652ff8e271e24
IOhannes m zmölnig <zmoelnig@iem.at>
2026-05-06 14:21:26 +0200

    compat with TclTLS-1.8 and TclTLS-2.0
    Closes: https://github.com/pure-data/deken/issues/317

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -28,10 +28,10 @@ package require Tcl 8.4 9
 
 package require http 2
 # try enabling https if possible
-if { [catch {package require tls 1.7} ] } {} else {
+if { [catch {package require tls 1.7-} ] } {} else {
     ::tls::init \
         -autoservername 1 \
-        -ssl2 false -ssl3 false -tls1 true
+        -tls1 false -tls1.1 false
     ::http::register https 443 ::tls::socket
 }
 # try enabling PROXY support if possible
