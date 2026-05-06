b172debdc2a1c3c0dbf9f5d6c16fe1a8a7da3da3
IOhannes m zmölnig <zmoelnig@iem.at>
2026-05-06 14:29:09 +0200

    Releasing v0.10.12

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -108,7 +108,7 @@ proc ::deken::versioncheck {version} {
 }
 
 ## put the current version of this package here:
-if { [::deken::versioncheck 0.10.11] } {
+if { [::deken::versioncheck 0.10.12] } {
 
 namespace eval ::deken:: {
     namespace export open_searchui
