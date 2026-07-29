6d0486803c87cae97ff08c605fd6cac1c412c7a7
IOhannes m zmölnig <zmoelnig@umlautT.umlaeute.mur.at>
2026-07-29 23:00:16 +0200

    deken-plugin: drop explicit dependency on Tcl>=8.4 || Tcl>=9
    Pd already depends on this...

--- deken-plugin.tcl
+++ deken-plugin.tcl
@@ -17,10 +17,6 @@
 ## - redirect ::deken::post to ::pdwindow::post (that is: use the results pane only for results)
 ## + make the "add to path" thingy configurable
 
-# The minimum version of TCL that allows the plugin to run
-package require Tcl 8.4 9
-# If Tk or Ttk is needed
-#package require Ttk
 # Any elements of the Pd GUI that are required
 # + require everything and all your script needs.
 #   If a requirement is missing,
